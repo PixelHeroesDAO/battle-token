644246bc2329037b34b2b621b4efd8d481e06c2a
cryptedy <edy.sassolino@gmail.com>
2023-10-22 23:35:31 +0900

    Use mock for test

--- test/PHBTInit.t.sol
+++ test/PHBTInit.t.sol
@@ -8,30 +8,40 @@ import "diamond-2-hardhat/Diamond.sol";
 import "diamond-2-hardhat/facets/DiamondLoupeFacet.sol";
 import "diamond-2-hardhat/facets/DiamondCutFacet.sol";
 import "diamond-2-hardhat/facets/OwnershipFacet.sol";
+import "solidity-examples/mocks/LZEndpointMock.sol";
 
-import "../src/OFT/interfaces/ILayerZeroEndpoint.sol";
+//import "../src/OFT/interfaces/ILayerZeroEndpoint.sol";
 
 contract TestPHBTInit is Test {
     string constant FN_ENDPOINT = "./bytecode/Endpoint";
+    uint16 constant CHAIN_ID = 1221;
+    address constant DEPLOYER_ENDPOINT = address(0x01);
     Diamond public diamond;
     DiamondCutFacet public diamondCutFacet;
     DiamondLoupeFacet public diamondLoupeFacet;
     OwnershipFacet public ownershipFacet;
     DiamondInit public diamondInit;
-    ILayerZeroEndpoint public endpoint;
+    LZEndpointMock public endpoint;
     function setUp() public {
+        // Prepare ethers for depolyer
+        vm.deal(DEPLOYER_ENDPOINT, 100 ether);
+
+        // Deploy endpoint
+        vm.prank(DEPLOYER_ENDPOINT);
+        endpoint = new LZEndpointMock(CHAIN_ID);
+
         diamondCutFacet = new DiamondCutFacet();
         diamondLoupeFacet = new DiamondLoupeFacet();
         diamond = new Diamond(address(this), address(this));
-        endpoint = ILayerZeroEndpoint(create2(vm.readFileBinary(FN_ENDPOINT), 0));
     }
     // deploy DiamondCutFacet
 
     function testTrial() public {
         console.log(address(endpoint));
-        console.log(precomputeCreate2(vm.readFileBinary(FN_ENDPOINT), 0));
+        console.log(endpoint.mockChainId());
     }
 
+    /*
     function create2(bytes memory bytecode, uint256 salt) internal returns (address child) {
         assembly {
             child := create2(0, add(bytecode, 32), mload(bytecode), salt)
@@ -43,4 +53,5 @@ contract TestPHBTInit is Test {
         bytes32 _data = keccak256(abi.encodePacked(bytes1(0xff), address(this), salt, bytecodeHash));
         return address(bytes20(_data << 96));
     }
+    */
 }
